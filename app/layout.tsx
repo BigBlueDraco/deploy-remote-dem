6da11360708a8417f997e4821ceac7e480ac14c0
BigBlueDraco <Dafarlog@gmail.com>
2023-07-31 17:55:42 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import "./globals.css";
+import "./normalize.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+
+const open_sans = Open_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={open_sans.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Projects from "@/components/Projects/Projects";
+import Tasks from "@/components/Tasks/Tasks";
+
+export default function Home() {
+  return (
+    <main>
+      <Tasks />
+      <Projects />
+    </main>
+  );
+}
